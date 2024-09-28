23954983d239b4fd00482d76fa7727f015661369
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-28 23:26:47 +0200

    Added Nav Links to Navigation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { ThemeSwitchComponent } from "../theme-switch/theme-switch.component";
+import { ThemeSwitchComponent } from '../theme-switch/theme-switch.component';
+import { NavLinkComponent } from '../nav-link/nav-link.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ThemeSwitchComponent],
+  imports: [ThemeSwitchComponent, NavLinkComponent],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {}
