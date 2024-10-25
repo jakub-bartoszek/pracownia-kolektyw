3a19698421ebed210700f779b54498e4bec7f762
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-10-25 15:30:05 +0200

    Added artist id route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { ArtistsComponent } from './pages/artists/artists.component';
+import { ArtistDetailComponent } from './pages/artist-detail/artist-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'galeria', component: GalleryComponent },
   { path: 'artysci', component: ArtistsComponent },
+  { path: 'artysci/:id', component: ArtistDetailComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
