5f22109bf6bf40aab61e1ad81c4e14b9be234a92
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-11-07 18:55:36 +0100

    Created services page

---
+++ src/app/pages/services/services.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-services',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './services.component.html',
+})
+export class ServicesComponent {}
