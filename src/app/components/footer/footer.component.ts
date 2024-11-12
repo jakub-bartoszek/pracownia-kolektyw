cfaad35aee1e4d1bce20bfa1c813231aeda61425
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-11-12 20:55:56 +0100

    Created navigate and scroll function and changed router links to href

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,38 @@
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FacebookIconComponent } from '../../icons/facebook-icon/facebook-icon.component';
 import { InstagramIconComponent } from '../../icons/instagram-icon/instagram-icon.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FacebookIconComponent, InstagramIconComponent, RouterModule],
+  imports: [FacebookIconComponent, InstagramIconComponent],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-  @Input() scrollToSection!: (sectionId: string) => void;
+  constructor(private router: Router) {}
+
+  navigateAndScrollToSection(sectionId: string) {
+    if (this.router.url === '/') {
+      this.scrollToSection(sectionId);
+    } else {
+      this.router.navigate(['/']).then(() => {
+        setTimeout(() => {
+          this.scrollToSection(sectionId);
+        }, 300);
+      });
+    }
+  }
+
+  private scrollToSection(sectionName: string) {
+    const section = document.getElementById(sectionName);
+    if (section) {
+      const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = sectionTop - 80;
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    }
+  }
 }
