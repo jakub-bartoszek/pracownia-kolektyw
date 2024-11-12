b17148677a4bc3d86aa8bdbf85b969148f77724d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-11-12 20:55:32 +0100

    Removed scroll config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {
-  InMemoryScrollingFeature,
-  InMemoryScrollingOptions,
-  provideRouter,
-  withInMemoryScrolling,
-} from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
@@ -20,18 +15,10 @@ const firebaseConfig = {
   appId: '1:724265975052:web:d36baaa614ea972c691d29',
 };
 
-const scrollConfig: InMemoryScrollingOptions = {
-  scrollPositionRestoration: 'top',
-  anchorScrolling: 'enabled',
-};
-
-const inMemoryScrollingFeature: InMemoryScrollingFeature =
-  withInMemoryScrolling(scrollConfig);
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, inMemoryScrollingFeature),
+    provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
