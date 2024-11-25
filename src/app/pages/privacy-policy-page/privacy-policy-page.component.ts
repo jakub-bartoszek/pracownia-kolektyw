e437d9a6d1b24b1d0e9ef41f03285ffaaa6a2228
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-11-25 18:33:35 +0100

    Improved consistency of styles

--- src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
+++ src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -1,9 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-privacy-policy-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacy-policy-page.component.html',
 })
-export class PrivacyPolicyPageComponent {}
+export class PrivacyPolicyPageComponent {
+  privacyPolicyList = [
+    {
+      title: 'Zakres Zbieranych Danych',
+      content:
+        'Na potrzeby założenia konta na stronie internetowej oraz napisania opinii zbieramy następujące dane osobowe: Imię i nazwisko, Adres e-mail.',
+    },
+    {
+      title: 'Cel Zbierania Danych',
+      content:
+        'Dane osobowe zbierane są wyłącznie w celu umożliwienia użytkownikom założenia konta i wystawiania opinii na stronie salonu.',
+    },
+    {
+      title: 'Okres Przechowywania Danych',
+      content:
+        'Dane osobowe przechowywane są przez czas istnienia konta użytkownika. W momencie usunięcia konta dane są natychmiastowo usuwane z naszej bazy.',
+    },
+    {
+      title: 'Udostępnianie Danych Osobowych',
+      content:
+        'Zebrane dane osobowe nie są udostępniane podmiotom trzecim i służą wyłącznie do celów wskazanych powyżej.',
+    },
+    {
+      title: 'Prawa Użytkownika',
+      content:
+        'Użytkownik ma prawo do dostępu do swoich danych, ich edycji oraz trwałego usunięcia poprzez usunięcie konta na stronie. Wszelkie prośby dotyczące zarządzania danymi osobowymi można zgłaszać za pośrednictwem formularza kontaktowego na stronie.',
+    },
+    {
+      title: 'Kontakt',
+      content:
+        'W razie jakichkolwiek pytań lub wątpliwości dotyczących ochrony danych osobowych prosimy o kontakt za pośrednictwem formularza dostępnego na stronie internetowej salonu.',
+    },
+  ];
+}
--- src/app/pages/tatoo-aftercare-page/tatoo-aftercare-page.component.ts
+++ src/app/pages/tatoo-aftercare-page/tatoo-aftercare-page.component.ts
@@ -1,11 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tatoo-aftercare-page',
   standalone: true,
-  imports: [],
-  templateUrl: './tatoo-aftercare-page.component.html'
+  imports: [CommonModule],
+  templateUrl: './tatoo-aftercare-page.component.html',
 })
 export class TatooAftercarePageComponent {
-
+  aftercareTips = [
+    {
+      title: 'Pielęgnacja tatuażu ze sztuczną skórą:',
+      content: [
+        'Zdejmij sztuczną skórę po 2-3 dniach pod bieżącą letnią wodą.',
+        'Oczyść tatuaż letnią wodą z antybakteryjnym mydłem lub płynem do higieny intymnej, a następnie osusz czystym ręcznikiem papierowym.',
+        'Nałóż cienką warstwę specjalnego kremu do tatuaży.',
+        'Unikaj bezpośredniego słońca, solarium i sauny.',
+      ],
+    },
+    {
+      title: 'Pielęgnacja tatuażu z folią spożywczą:',
+      content: [
+        'Zmieniać folię co kilka godzin, szczególnie jeśli się zamoczy.',
+        'Przed nałożeniem nowej folii delikatnie umyj tatuaż letnią wodą.',
+        'Po każdym oczyszczeniu nałóż cienką warstwę kremu do tatuaży.',
+        'Unikaj słońca, solarium i sauny.',
+      ],
+    },
+    {
+      title: 'Ogólne zasady pielęgnacji:',
+      content: [
+        'Myj i nawilżaj tatuaż kilka razy dziennie, szczególnie po wysiłku fizycznym.',
+        'Używaj wyłącznie produktów przeznaczonych do pielęgnacji tatuaży. Unikaj kosmetyków zawierających alkohol, perfumy i barwniki.',
+        'Noś luźne ubrania, które nie będą ocierać o tatuaż.',
+        'Unikaj miejsc o dużym zapyleniu i zanieczyszczeniu.',
+        'Nie drap ani nie ścieraj suchej skóry.',
+        'Unikaj intensywnych ćwiczeń, które powodują pocenie się w okolicy tatuażu.',
+      ],
+    },
+    {
+      title: 'Czego unikać:',
+      content: [
+        'Unikaj promieniowania UV, które może spowodować blaknięcie tatuażu.',
+        'Nie korzystaj z basenów i zbiorników wodnych.',
+        'Unikaj długich kąpieli, które mogą spowolnić proces gojenia.',
+      ],
+    },
+    {
+      title: 'Kiedy zgłosić się do tatuatora?',
+      content: [
+        'Jeśli zauważysz niepokojące objawy, takie jak silny ból, zaczerwienienie, obrzęk, wyciek ropy lub gorączka, natychmiast skontaktuj się z tatuatorem.',
+      ],
+    },
+  ];
 }
