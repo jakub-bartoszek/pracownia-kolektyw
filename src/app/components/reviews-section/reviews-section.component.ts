aa7619b9d5f42231c2cfd9b399784a798abea6d7
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-11-25 19:10:39 +0100

    Moved reviews section to its own component

---
+++ src/app/components/reviews-section/reviews-section.component.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ReviewsService } from '../../services/reviews.service';
+import { Review } from '../../models/models';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reviews-section',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './reviews-section.component.html',
+})
+export class ReviewsSectionComponent {
+  reviews: Review[] = [];
+  isLoggedIn = false;
+  isAdmin = false;
+  images: ImageData[] = [];
+  userReview: Review | null = null;
+  newReviewContent: string = '';
+  newReviewRate: number = 5;
+
+  constructor(
+    private authService: AuthService,
+    private reviewsService: ReviewsService
+  ) {}
+
+  openModal() {
+    this.authService.openAuthModal();
+  }
+
+  async addReview() {
+    try {
+      await this.reviewsService.submitReview(
+        this.newReviewContent,
+        this.newReviewRate
+      );
+      this.resetReviewFields();
+      this.loadUserReview();
+    } catch (error) {
+      console.error('Error adding review:', error);
+    }
+  }
+
+  async deleteReview(reviewId: string) {
+    try {
+      await this.reviewsService.removeReview(reviewId);
+      this.userReview = null;
+      this.loadReviews();
+    } catch (error) {
+      console.error('Error deleting review:', error);
+    }
+  }
+
+  async loadUserReview() {
+    this.authService.currentUser$.subscribe(async (user) => {
+      if (user) {
+        this.userReview = await this.reviewsService.loadUserReview(user.uid);
+      }
+    });
+  }
+
+  loadReviews() {
+    this.reviewsService.loadReviews().subscribe((reviews) => {
+      this.reviews = reviews;
+    });
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.loadReviews();
+    this.loadUserReview();
+
+    this.authService.isLoggedIn$.subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+
+    this.authService.isAdmin$.subscribe((admin) => {
+      this.isAdmin = admin;
+    });
+  }
+
+  private resetReviewFields() {
+    this.newReviewContent = '';
+    this.newReviewRate = 5;
+  }
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -9,6 +9,7 @@ import { GalleryService } from '../../services/gallery.service';
 import { ArtistsService } from '../../services/artists.service';
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 import { ArtistsSectionComponent } from '../../components/artists-section/artists-section.component';
+import { ReviewsSectionComponent } from '../../components/reviews-section/reviews-section.component';
 
 @Component({
   selector: 'app-home-page',
@@ -20,22 +21,18 @@ import { ArtistsSectionComponent } from '../../components/artists-section/artist
     RouterModule,
     ContactFormComponent,
     ArtistsSectionComponent,
+    ReviewsSectionComponent,
   ],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent implements OnInit {
-  reviews: Review[] = [];
   isLoggedIn = false;
   isAdmin = false;
   artists: Artist[] = [];
   images: ImageData[] = [];
-  userReview: Review | null = null;
-  newReviewContent: string = '';
-  newReviewRate: number = 5;
 
   constructor(
     private authService: AuthService,
-    private reviewsService: ReviewsService,
     private galleryService: GalleryService,
     private artistsService: ArtistsService
   ) {}
@@ -51,54 +48,13 @@ export class HomePageComponent implements OnInit {
       const offsetPosition = sectionTop - 80;
       window.scrollTo({
         top: offsetPosition,
-        behavior: 'smooth',
       });
     }
-  }
-
-  async addReview() {
-    try {
-      await this.reviewsService.submitReview(
-        this.newReviewContent,
-        this.newReviewRate
-      );
-      this.resetReviewFields();
-      this.loadUserReview();
-      this.loadReviews();
-    } catch (error) {
-      console.error('Error adding review:', error);
-    }
-  }
-
-  async deleteReview(reviewId: string) {
-    try {
-      await this.reviewsService.removeReview(reviewId);
-      this.userReview = null;
-      this.loadReviews();
-    } catch (error) {
-      console.error('Error deleting review:', error);
-    }
-  }
-
-  async loadUserReview() {
-    this.authService.currentUser$.subscribe(async (user) => {
-      if (user) {
-        this.userReview = await this.reviewsService.loadUserReview(user.uid);
-      }
-    });
-  }
-
-  loadReviews() {
-    this.reviewsService.loadReviews().subscribe((reviews) => {
-      this.reviews = reviews;
-    });
   }
 
   async ngOnInit(): Promise<void> {
     this.artists = await this.artistsService.loadArtists();
     this.images = await this.galleryService.loadImages();
-    this.loadReviews();
-    this.loadUserReview();
 
     this.authService.isLoggedIn$.subscribe((loggedIn) => {
       this.isLoggedIn = loggedIn;
@@ -111,10 +67,5 @@ export class HomePageComponent implements OnInit {
 
   getArtistFullName(artistId: string): string {
     return this.artistsService.getArtistFullName(artistId, this.artists);
-  }
-
-  private resetReviewFields() {
-    this.newReviewContent = '';
-    this.newReviewRate = 5;
   }
 }
