ce56b8ef9f067686068903baef959e357099ffdd
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-06 18:36:22 +0100

    Created separate layouts

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-admin-layout',
   standalone: true,
-  templateUrl: './app.component.html',
   imports: [RouterOutlet],
+  templateUrl: './admin-layout.component.html',
 })
-export class AppComponent {}
+export class AdminLayoutComponent {}
---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { AuthModalComponent } from '../../components/auth-modal/auth-modal.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent, RouterOutlet, AuthModalComponent, CommonModule],
+  templateUrl: './main-layout.component.html',
+})
+export class MainLayoutComponent {
+  isAuthModalOpen = false;
+
+  constructor(private authService: AuthService) {
+    this.authService.isAuthModalOpen$.subscribe((isOpen) => {
+      this.isAuthModalOpen = isOpen;
+    });
+  }
+
+  openAuthModal(): void {
+    this.authService.openAuthModal();
+  }
+
+  closeAuthModal(): void {
+    this.authService.closeAuthModal();
+  }
+
+  scrollToSection(sectionName: string): void {
+    const section = document.getElementById(sectionName);
+    if (section) {
+      const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = sectionTop - 80;
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    }
+  }
+}
