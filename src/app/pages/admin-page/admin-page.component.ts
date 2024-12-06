00c93b6277b2b61b030fe3d7c8e3ed2857706a3d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-06 18:36:47 +0100

    Created admin-page component

---
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-page.component.html'
+})
+export class AdminPageComponent {
+
+}
