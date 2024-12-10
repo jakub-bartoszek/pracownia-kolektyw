0625e61289c142e3d590388ee46e156aa7f1ef2f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-10 14:48:52 +0100

    Removed auth service logic and modal

--- src/app/pages/main/artists-page/artists-page.component.ts
+++ src/app/pages/main/artists-page/artists-page.component.ts
@@ -1,43 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AuthService } from '../../../services/auth.service';
-import { UploadArtistModalComponent } from '../../../components/main/upload-artist-modal/upload-artist-modal.component';
 import { ArtistsSectionComponent } from '../../../components/main/artists-section/artists-section.component';
 
 @Component({
   standalone: true,
-  imports: [
-    UploadArtistModalComponent,
-    CommonModule,
-    RouterModule,
-    ArtistsSectionComponent,
-  ],
+  imports: [CommonModule, RouterModule, ArtistsSectionComponent],
   selector: 'app-artists-page',
   templateUrl: './artists-page.component.html',
 })
-export class ArtistsPageComponent implements OnInit {
-  isModalOpen = false;
-  isAdmin = false;
-  isLoggedIn = false;
-
-  constructor(private authService: AuthService) {}
-
-  async ngOnInit(): Promise<void> {
-    this.authService.isLoggedIn$.subscribe((loggedIn) => {
-      this.isLoggedIn = loggedIn;
-    });
-
-    this.authService.isAdmin$.subscribe((admin) => {
-      this.isAdmin = admin;
-    });
-  }
-
-  openModal() {
-    this.isModalOpen = true;
-  }
-
-  closeModal() {
-    this.isModalOpen = false;
-  }
-}
+export class ArtistsPageComponent {}
