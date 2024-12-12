0412f06577bc9caa1cb2899e97284cb09164cd34
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-12 15:04:41 +0100

    Enabled scroll position restoration for router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,16 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+} from '@angular/router';
 import { routes } from './app.routes';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAgdnw4ucnZH3JvSudWKpHfXrsB_vHbugM',
@@ -15,13 +22,25 @@ const firebaseConfig = {
   appId: '1:724265975052:web:d36baaa614ea972c691d29',
 };
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes, inMemoryScrollingFeature),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
     provideAuth(() => getAuth()),
   ],
 };
+
+bootstrapApplication(AppComponent, {
+  providers: appConfig.providers,
+});
