5e2b68e991175280a29b3853b763f49f533347ec
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-12-13 18:43:18 +0100

    Added artworks section

--- src/app/pages/main/artist-page/artist-page.component.ts
+++ src/app/pages/main/artist-page/artist-page.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Artist, ImageData } from '../../../models/models';
 import { ArtistsService } from '../../../services/artists.service';
 import { GalleryService } from '../../../services/gallery.service';
+import { ImageModalComponent } from '../../../components/main/image-modal/image-modal.component';
 
 @Component({
   selector: 'app-artist-page',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ImageModalComponent, RouterModule],
   templateUrl: './artist-page.component.html',
 })
 export class ArtistPageComponent implements OnInit {
   artist: Artist | null = null;
   artworks: ImageData[] = [];
-  selectedProfileImageFile?: File;
+  selectedImageUrl: string = '';
+  isImageModalOpen: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private artistsService: ArtistsService,
     private galleryService: GalleryService
   ) {}
+
+  openImageModal(imageUrl: string) {
+    this.selectedImageUrl = imageUrl;
+    this.isImageModalOpen = true;
+  }
+
+  closeImageModal() {
+    this.isImageModalOpen = false;
+    this.selectedImageUrl = '';
+  }
 
   async ngOnInit(): Promise<void> {
     const id = this.route.snapshot.paramMap.get('id');
