0560e8eff2c3cbb2cd3563b5d9041ae6c79c12ec
Jakub Bartoszek <k.bartoszek507@wp.pl>
2025-01-04 22:42:22 +0100

    Added ability to change opacity of custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,60 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--color-background)",
-        "primary": "var(--color-primary)",
-        "accent": "var(--color-accent)",
+        background: ({ opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `rgba(var(--color-background-rgb), ${opacityValue})`;
+          }
+          return `rgb(var(--color-background-rgb))`;
+        },
+        primary: ({ opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `rgba(var(--color-primary-rgb), ${opacityValue})`;
+          }
+          return `rgb(var(--color-primary-rgb))`;
+        },
+        accent: ({ opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `rgba(var(--color-accent-rgb), ${opacityValue})`;
+          }
+          return `rgb(var(--color-accent-rgb))`;
+        },
+        muted: ({ opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `rgba(var(--color-primary-rgb), 0.5)`; // Zawsze 50% przezroczystości
+          }
+          return `rgba(var(--color-primary-rgb), 0.5)`; // Domyślna wartość
+        },
       },
-    },
-    fontFamily: {
-      sans: ['Roboto', 'Arial', 'sans-serif'],
+      backgroundImage: {
+        'hero-pattern': "url('/images/banner.png')",
+      },
+      animation: {
+        fadeInUp: "fadeInUp 1s ease-out forwards",
+        fadeInDown: "fadeInDown 2.5s ease-out forwards",
+      },
+      keyframes: {
+        fadeInUp: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+        fadeInDown: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+      },
     },
   },
   plugins: [],
