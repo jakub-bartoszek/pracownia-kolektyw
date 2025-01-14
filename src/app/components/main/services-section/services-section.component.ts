daddc728e6ca04de71791e73a6d840664172245c
Jakub Bartoszek <k.bartoszek507@wp.pl>
2025-01-14 18:55:21 +0100

    Imported RouterModule

--- src/app/components/main/services-section/services-section.component.ts
+++ src/app/components/main/services-section/services-section.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-services-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './services-section.component.html',
 })
 export class ServicesSectionComponent {
